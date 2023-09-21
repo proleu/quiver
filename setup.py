5473bbf39b19014fbce013d6eb26e6343d82a2c0
Philip <philipjleung@gmail.com>
2023-09-21 15:42:32 -0700

    run black

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
+import os
 from setuptools import setup, find_packages
+
+executables = [
+    f for f in os.listdir(".") if os.path.isfile(f) and os.access(f, os.X_OK)
+]
 
 setup(
     name="quiver",
     version="0.1",
     packages=find_packages(),
+    scripts=executables,
 )
--- test/gen_test_input/gen_scored_qv.py
+++ test/gen_test_input/gen_scored_qv.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python3
 
-'''
+"""
     Take 10 pdbs from the ../input_for_tests dir then assign them random scores and stash this in a Quiver file.
     This scores quiver file will be used in testing later.
 
-'''
+"""
 
 import sys
-sys.path.append('../../')
+
+sys.path.append("../../")
 from quiver import Quiver
 import os
 import glob
 import random
 
 # Make a quiver file
-qv = Quiver('test.qv', 'w')
+qv = Quiver("test.qv", "w")
 
 for i in range(10):
     # Get a random pdb from the input_for_tests dir
-    pdbs = glob.glob('../input_for_tests/*.pdb')
+    pdbs = glob.glob("../input_for_tests/*.pdb")
     pdb = pdbs[i]
     # Get the pdb name
     pdb_name = os.path.basename(pdb)
 
-    pdblines = open(pdb, 'r').readlines()
+    pdblines = open(pdb, "r").readlines()
 
     # Remove the .pdb extension
     tag = pdb_name[:-4]
 
     # Make up some random scores
 
-    ddg    = random.randint(-100,100)
-    charge = random.randint(-100,100)
-    cool   = random.randint(-100,100)
-    scoreX = random.randint(-100,100)
+    ddg = random.randint(-100, 100)
+    charge = random.randint(-100, 100)
+    cool = random.randint(-100, 100)
+    scoreX = random.randint(-100, 100)
 
-    score_str = f'ddg={ddg}|charge={charge}|cool={cool}|scoreX={scoreX}'
-    
+    score_str = f"ddg={ddg}|charge={charge}|cool={cool}|scoreX={scoreX}"
+
     # Add the pdb to the quiver file
     qv.add_pdb(pdblines, tag, score_str)
